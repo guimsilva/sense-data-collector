a07bfeed722a08b9cd6fabff960c266978dad3c5
guimsilva <29346899+guimsilva@users.noreply.github.com>
2024-11-30 15:32:18 -0300

    Some reformating

--- src/elev.ino
+++ src/elev.ino
@@ -24,41 +24,43 @@ limitations under the License.
 
 #define BLE_SENSE_UUID(val) ("4798e0f2-" val "-4d68-af64-8a8f5258404e")
 
-namespace {
-const int VERSION = 0x00000000;
+namespace
+{
+  const int VERSION = 0x00000000;
 
-// Constants for image rasterization
-constexpr int raster_width = 32;
-constexpr int raster_height = 32;
-constexpr int raster_channels = 3;
-constexpr int raster_byte_count = raster_height * raster_width * raster_channels;
-int8_t raster_buffer[raster_byte_count];
+  // Constants for image rasterization
+  constexpr int raster_width = 32;
+  constexpr int raster_height = 32;
+  constexpr int raster_channels = 3;
+  constexpr int raster_byte_count = raster_height * raster_width * raster_channels;
+  int8_t raster_buffer[raster_byte_count];
 
-// BLE settings
-BLEService service(BLE_SENSE_UUID("0000"));
-BLECharacteristic strokeCharacteristic(BLE_SENSE_UUID("300a"), BLERead, stroke_struct_byte_count);
+  // BLE settings
+  BLEService service(BLE_SENSE_UUID("0000"));
+  BLECharacteristic strokeCharacteristic(BLE_SENSE_UUID("300a"), BLERead, stroke_struct_byte_count);
 
-// String to calculate the local and device name
-String name;
+  // String to calculate the local and device name
+  String name;
 
-// Create an area of memory to use for input, output, and intermediate arrays.
-// The size of this will depend on the model you're using, and may need to be
-// determined by experimentation.
-constexpr int kTensorArenaSize = 30 * 1024;
-uint8_t tensor_arena[kTensorArenaSize];
+  // Create an area of memory to use for input, output, and intermediate arrays.
+  // The size of this will depend on the model you're using, and may need to be
+  // determined by experimentation.
+  constexpr int kTensorArenaSize = 30 * 1024;
+  uint8_t tensor_arena[kTensorArenaSize];
 
-tflite::ErrorReporter* error_reporter = nullptr;
-const tflite::Model* model = nullptr;
-tflite::MicroInterpreter* interpreter = nullptr;
+  tflite::ErrorReporter *error_reporter = nullptr;
+  const tflite::Model *model = nullptr;
+  tflite::MicroInterpreter *interpreter = nullptr;
 
-// -------------------------------------------------------------------------------- //
-// UPDATE THESE VARIABLES TO MATCH THE NUMBER AND LIST OF GESTURES IN YOUR DATASET  //
-// -------------------------------------------------------------------------------- //
-constexpr int label_count = 10;
-const char* labels[label_count] = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-}  // namespace
+  // -------------------------------------------------------------------------------- //
+  // UPDATE THESE VARIABLES TO MATCH THE NUMBER AND LIST OF GESTURES IN YOUR DATASET  //
+  // -------------------------------------------------------------------------------- //
+  constexpr int label_count = 10;
+  const char *labels[label_count] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+} // namespace
 
-void setup() {
+void setup()
+{
   // Start serial
   Serial.begin(9600);
   while (!Serial)
@@ -66,7 +68,8 @@ void setup() {
   Serial.println("Started");
 
   // Start IMU
-  if (!IMU.begin()) {
+  if (!IMU.begin())
+  {
     Serial.println("Failed to initialized IMU!");
     while (1)
       ;
@@ -74,7 +77,8 @@ void setup() {
   SetupIMU();
 
   // Start BLE
-  if (!BLE.begin()) {
+  if (!BLE.begin())
+  {
     Serial.println("Failed to initialized BLE!");
     while (1)
       ;
@@ -105,16 +109,16 @@ void setup() {
   BLE.addService(service);
   BLE.advertise();
 
-
   // Set up logging. Google style is to avoid globals or statics because of
   // lifetime uncertainty, but since this has a trivial destructor it's okay.
-  static tflite::MicroErrorReporter micro_error_reporter;  // NOLINT
+  static tflite::MicroErrorReporter micro_error_reporter; // NOLINT
   error_reporter = &micro_error_reporter;
 
   // Map the model into a usable data structure. This doesn't involve any
   // copying or parsing, it's a very lightweight operation.
   model = tflite::GetModel(g_vibration_model_data);
-  if (model->version() != TFLITE_SCHEMA_VERSION) {
+  if (model->version() != TFLITE_SCHEMA_VERSION)
+  {
     TF_LITE_REPORT_ERROR(error_reporter,
                          "Model provided is schema version %d not equal "
                          "to supported version %d.",
@@ -127,7 +131,7 @@ void setup() {
   // An easier approach is to just use the AllOpsResolver, but this will
   // incur some penalty in code space for op implementations that are not
   // needed by this graph.
-  static tflite::MicroMutableOpResolver<4> micro_op_resolver;  // NOLINT
+  static tflite::MicroMutableOpResolver<4> micro_op_resolver; // NOLINT
   micro_op_resolver.AddConv2D();
   micro_op_resolver.AddMean();
   micro_op_resolver.AddFullyConnected();
@@ -135,35 +139,39 @@ void setup() {
 
   // Build an interpreter to run the model with.
   static tflite::MicroInterpreter static_interpreter(
-    model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+      model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
   interpreter = &static_interpreter;
 
   // Allocate memory from the tensor_arena for the model's tensors.
   interpreter->AllocateTensors();
 
   // Set model input settings
-  TfLiteTensor* model_input = interpreter->input(0);
-  if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) || (model_input->dims->data[1] != raster_height) || (model_input->dims->data[2] != raster_width) || (model_input->dims->data[3] != raster_channels) || (model_input->type != kTfLiteInt8)) {
+  TfLiteTensor *model_input = interpreter->input(0);
+  if ((model_input->dims->size != 4) || (model_input->dims->data[0] != 1) || (model_input->dims->data[1] != raster_height) || (model_input->dims->data[2] != raster_width) || (model_input->dims->data[3] != raster_channels) || (model_input->type != kTfLiteInt8))
+  {
     TF_LITE_REPORT_ERROR(error_reporter,
                          "Bad input tensor parameters in model");
     return;
   }
 
   // Set model output settings
-  TfLiteTensor* model_output = interpreter->output(0);
-  if ((model_output->dims->size != 2) || (model_output->dims->data[0] != 1) || (model_output->dims->data[1] != label_count) || (model_output->type != kTfLiteInt8)) {
+  TfLiteTensor *model_output = interpreter->output(0);
+  if ((model_output->dims->size != 2) || (model_output->dims->data[0] != 1) || (model_output->dims->data[1] != label_count) || (model_output->type != kTfLiteInt8))
+  {
     TF_LITE_REPORT_ERROR(error_reporter,
                          "Bad output tensor parameters in model");
     return;
   }
 }
 
-void loop() {
+void loop()
+{
   BLEDevice central = BLE.central();
 
   // if a central is connected to the peripheral:
   static bool was_connected_last = false;
-  if (central && !was_connected_last) {
+  if (central && !was_connected_last)
+  {
     Serial.print("Connected to central: ");
     // print the central's BT address:
     Serial.println(central.address());
@@ -173,7 +181,8 @@ void loop() {
   // make sure IMU data is available then read in data
   // Serial.println("Checking acc and gyr availability...");
   const bool data_available = IMU.accelerationAvailable() || IMU.gyroscopeAvailable();
-  if (!data_available) {
+  if (!data_available)
+  {
     // Serial.println("NO DATA AVAILABLE");
     return;
   }
@@ -183,41 +192,54 @@ void loop() {
 
   // Parse and process IMU data
   bool done_just_triggered = false;
-  if (gyroscope_samples_read > 0) {
+  if (gyroscope_samples_read > 0)
+  {
     Serial.println(">>> gyroscope_samples_read: " + String(gyroscope_samples_read));
     EstimateGyroscopeDrift(current_gyroscope_drift);
     UpdateOrientation(gyroscope_samples_read, current_gravity, current_gyroscope_drift);
     UpdateStroke(gyroscope_samples_read, &done_just_triggered);
-    if (central && central.connected()) {
+    if (central && central.connected())
+    {
       strokeCharacteristic.writeValue(stroke_struct_buffer, stroke_struct_byte_count);
     }
-  } else {
+  }
+  else
+  {
     // Serial.println("NO gyroscope_samples_read");
   }
-  if (accelerometer_samples_read > 0) {
+  if (accelerometer_samples_read > 0)
+  {
     Serial.println(">>> accelerometer_samples_read: " + String(accelerometer_samples_read));
     EstimateGravityDirection(current_gravity);
     UpdateVelocity(accelerometer_samples_read, current_gravity);
-  } else {
+  }
+  else
+  {
     // Serial.println("NO accelerometer_samples_read");
   }
 
   // Wait for a gesture to be done
-  if (done_just_triggered) {
+  if (done_just_triggered)
+  {
     Serial.println(">>> done_just_triggered");
     // Rasterize the gesture
     RasterizeStroke(stroke_points, *stroke_transmit_length, 0.6f, 0.6f, raster_width, raster_height, raster_buffer);
-    for (int y = 0; y < raster_height; ++y) {
+    for (int y = 0; y < raster_height; ++y)
+    {
       char line[raster_width + 1];
-      for (int x = 0; x < raster_width; ++x) {
-        const int8_t* pixel = &raster_buffer[(y * raster_width * raster_channels) + (x * raster_channels)];
+      for (int x = 0; x < raster_width; ++x)
+      {
+        const int8_t *pixel = &raster_buffer[(y * raster_width * raster_channels) + (x * raster_channels)];
         const int8_t red = pixel[0];
         const int8_t green = pixel[1];
         const int8_t blue = pixel[2];
         char output;
-        if ((red > -128) || (green > -128) || (blue > -128)) {
+        if ((red > -128) || (green > -128) || (blue > -128))
+        {
           output = '#';
-        } else {
+        }
+        else
+        {
           output = '.';
         }
         line[x] = output;
@@ -227,29 +249,35 @@ void loop() {
     }
 
     // Pass to the model and run the interpreter
-    TfLiteTensor* model_input = interpreter->input(0);
-    for (int i = 0; i < raster_byte_count; ++i) {
+    TfLiteTensor *model_input = interpreter->input(0);
+    for (int i = 0; i < raster_byte_count; ++i)
+    {
       model_input->data.int8[i] = raster_buffer[i];
     }
     TfLiteStatus invoke_status = interpreter->Invoke();
-    if (invoke_status != kTfLiteOk) {
+    if (invoke_status != kTfLiteOk)
+    {
       TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed");
       return;
     }
-    TfLiteTensor* output = interpreter->output(0);
+    TfLiteTensor *output = interpreter->output(0);
 
     // Parse the model output
     int8_t max_score;
     int max_index;
-    for (int i = 0; i < label_count; ++i) {
+    for (int i = 0; i < label_count; ++i)
+    {
       const int8_t score = output->data.int8[i];
-      if ((i == 0) || (score > max_score)) {
+      if ((i == 0) || (score > max_score))
+      {
         max_score = score;
         max_index = i;
       }
     }
     TF_LITE_REPORT_ERROR(error_reporter, "Found %s (%d)", labels[max_index], max_score);
-  } else {
+  }
+  else
+  {
     Serial.println("NOT done_just_triggered");
   }
 }
--- src/rasterize_stroke.cpp
+++ src/rasterize_stroke.cpp
@@ -12,67 +12,86 @@ limitations under the License.
 
 #include "rasterize_stroke.h"
 
-namespace {
-constexpr int kFixedPoint = 256;
+namespace
+{
+  constexpr int kFixedPoint = 256;
 
-int32_t MulFP(int32_t a, int32_t b) {
-  return (a * b) / kFixedPoint;
-}
-
-int32_t DivFP(int32_t a, int32_t b) {
-  if (b == 0) {
-    b = 1;
+  int32_t MulFP(int32_t a, int32_t b)
+  {
+    return (a * b) / kFixedPoint;
   }
-  return (a * kFixedPoint) / b;
-}
 
-int32_t FloatToFP(float a) {
-  return static_cast<int32_t>(a * kFixedPoint);
-}
-
-int32_t NormToCoordFP(int32_t a_fp, int32_t range_fp, int32_t half_size_fp) {
-  const int32_t norm_fp = DivFP(a_fp, range_fp);
-  return MulFP(norm_fp, half_size_fp) + half_size_fp;
-}
-
-int32_t RoundFPToInt(int32_t a) {
-  return static_cast<int32_t>((a + (kFixedPoint / 2)) / kFixedPoint);
-}
-
-int32_t Gate(int32_t a, int32_t min, int32_t max) {
-  if (a < min) {
-    return min;
-  } else if (a > max) {
-    return max;
-  } else {
-    return a;
+  int32_t DivFP(int32_t a, int32_t b)
+  {
+    if (b == 0)
+    {
+      b = 1;
+    }
+    return (a * kFixedPoint) / b;
   }
-}
 
-int32_t Abs(int32_t a) {
-  if (a > 0) {
-    return a;
-  } else {
-    return -a;
+  int32_t FloatToFP(float a)
+  {
+    return static_cast<int32_t>(a * kFixedPoint);
   }
-}
 
-}  // namespace
+  int32_t NormToCoordFP(int32_t a_fp, int32_t range_fp, int32_t half_size_fp)
+  {
+    const int32_t norm_fp = DivFP(a_fp, range_fp);
+    return MulFP(norm_fp, half_size_fp) + half_size_fp;
+  }
+
+  int32_t RoundFPToInt(int32_t a)
+  {
+    return static_cast<int32_t>((a + (kFixedPoint / 2)) / kFixedPoint);
+  }
+
+  int32_t Gate(int32_t a, int32_t min, int32_t max)
+  {
+    if (a < min)
+    {
+      return min;
+    }
+    else if (a > max)
+    {
+      return max;
+    }
+    else
+    {
+      return a;
+    }
+  }
+
+  int32_t Abs(int32_t a)
+  {
+    if (a > 0)
+    {
+      return a;
+    }
+    else
+    {
+      return -a;
+    }
+  }
+
+} // namespace
 
 void RasterizeStroke(
-    int8_t* stroke_points,
+    int8_t *stroke_points,
     int stroke_points_count,
-    float x_range, 
-    float y_range, 
-    int width, 
+    float x_range,
+    float y_range,
+    int width,
     int height,
-    int8_t* out_buffer) {
-  //Convert stroke (2d coordinates of the gesture) into a 2d color image 
+    int8_t *out_buffer)
+{
+  // Convert stroke (2d coordinates of the gesture) into a 2d color image
   constexpr int num_channels = 3;
   const int buffer_byte_count = height * width * num_channels;
 
-  //initialize all pixels to black
-  for (int i = 0; i < buffer_byte_count; ++i) {
+  // initialize all pixels to black
+  for (int i = 0; i < buffer_byte_count; ++i)
+  {
     out_buffer[i] = -128;
   }
 
@@ -87,15 +106,16 @@ void RasterizeStroke(
 
   const int one_half_fp = (kFixedPoint / 2);
 
-  for (int point_index = 0; point_index < (stroke_points_count - 1); ++point_index) {
-    //Iterate through the stroke and sELEVt two sequential start and end coordinate pairs
-    // to form the a line segment
-    //example: Stroke points = [a,b,c...] then we would iterate start=a end=b, start=b end=c...
-    const int8_t* start_point = &stroke_points[point_index * 2];
+  for (int point_index = 0; point_index < (stroke_points_count - 1); ++point_index)
+  {
+    // Iterate through the stroke and sELEVt two sequential start and end coordinate pairs
+    //  to form the a line segment
+    // example: Stroke points = [a,b,c...] then we would iterate start=a end=b, start=b end=c...
+    const int8_t *start_point = &stroke_points[point_index * 2];
     const int32_t start_point_x_fp = (start_point[0] * kFixedPoint) / 128;
     const int32_t start_point_y_fp = (start_point[1] * kFixedPoint) / 128;
 
-    const int8_t* end_point = &stroke_points[(point_index + 1) * 2];
+    const int8_t *end_point = &stroke_points[(point_index + 1) * 2];
     const int32_t end_point_x_fp = (end_point[0] * kFixedPoint) / 128;
     const int32_t end_point_y_fp = (end_point[1] * kFixedPoint) / 128;
 
@@ -106,19 +126,22 @@ void RasterizeStroke(
     const int32_t delta_x_fp = end_x_fp - start_x_fp;
     const int32_t delta_y_fp = end_y_fp - start_y_fp;
 
-    //assign the color of the pixels of this line segment
-    // the color shifts from red->green->blue as we get later in the gesture
+    // assign the color of the pixels of this line segment
+    //  the color shifts from red->green->blue as we get later in the gesture
     const int32_t t_fp = point_index * t_inc_fp;
     int32_t red_i32;
     int32_t green_i32;
     int32_t blue_i32;
-    if (t_fp < one_half_fp) {
+    if (t_fp < one_half_fp)
+    {
       const int32_t local_t_fp = DivFP(t_fp, one_half_fp);
       const int32_t one_minus_t_fp = kFixedPoint - local_t_fp;
       red_i32 = RoundFPToInt(one_minus_t_fp * 255) - 128;
       green_i32 = RoundFPToInt(local_t_fp * 255) - 128;
       blue_i32 = -128;
-    } else {
+    }
+    else
+    {
       const int32_t local_t_fp = DivFP(t_fp - one_half_fp, one_half_fp);
       const int32_t one_minus_t_fp = kFixedPoint - local_t_fp;
       red_i32 = -128;
@@ -132,32 +155,43 @@ void RasterizeStroke(
     int line_length;
     int32_t x_inc_fp;
     int32_t y_inc_fp;
-    if (Abs(delta_x_fp) > Abs(delta_y_fp)) {
+    if (Abs(delta_x_fp) > Abs(delta_y_fp))
+    {
       line_length = Abs(RoundFPToInt(delta_x_fp));
-      if (delta_x_fp > 0) {
+      if (delta_x_fp > 0)
+      {
         x_inc_fp = 1 * kFixedPoint;
         y_inc_fp = DivFP(delta_y_fp, delta_x_fp);
-      } else {
+      }
+      else
+      {
         x_inc_fp = -1 * kFixedPoint;
         y_inc_fp = -DivFP(delta_y_fp, delta_x_fp);
       }
-    } else {
+    }
+    else
+    {
       line_length = Abs(RoundFPToInt(delta_y_fp));
-      if (delta_y_fp > 0) {
+      if (delta_y_fp > 0)
+      {
         y_inc_fp = 1 * kFixedPoint;
         x_inc_fp = DivFP(delta_x_fp, delta_y_fp);
-      } else {
+      }
+      else
+      {
         y_inc_fp = -1 * kFixedPoint;
         x_inc_fp = -DivFP(delta_x_fp, delta_y_fp);
       }
     }
-    for (int i = 0; i < (line_length + 1); ++i) {
-      //iterate through the line segment and assign the pixel values
+    for (int i = 0; i < (line_length + 1); ++i)
+    {
+      // iterate through the line segment and assign the pixel values
       const int32_t x_fp = start_x_fp + (i * x_inc_fp);
       const int32_t y_fp = start_y_fp + (i * y_inc_fp);
       const int x = RoundFPToInt(x_fp);
       const int y = RoundFPToInt(y_fp);
-      if ((x < 0) or (x >= width) or (y < 0) or (y >= height)) {
+      if ((x < 0) or (x >= width) or (y < 0) or (y >= height))
+      {
         continue;
       }
       const int buffer_index = (y * width * num_channels) + (x * num_channels);
